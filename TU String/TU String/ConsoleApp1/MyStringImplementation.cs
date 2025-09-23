83d78cdfad4d81349cd5beb287c8170b7fa37790
AlexandreGlatz <alexandre.glatzpro@gmail.com>
2025-09-23 12:40:19 +0200

    [ADD] String exercises

---
+++ TU String/TU String/ConsoleApp1/MyStringImplementation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TU_Challenge
+{
+    public class MyStringImplementation
+    {
+        public static bool IsNullEmptyOrWhiteSpace(string? input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string MixString(string s, string s1)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ToLowerCase(string s)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string Voyelles(string s)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string Reverse(string s)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string BazardString(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string UnBazardString(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ToCesarCode(string input, int offset)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
